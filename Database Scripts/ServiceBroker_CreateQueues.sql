c3db84c99f08781481ef0398cffb62c8f8988fe3
Anton Georgescu <antongeorgescu@gmail.com>
2021-03-13 17:01:29 -0500

    Update ServiceBroker_CreateQueues.sql

--- Database Scripts/ServiceBroker_CreateQueues.sql
+++ Database Scripts/ServiceBroker_CreateQueues.sql
@@ -9,3 +9,13 @@ GO
 CREATE QUEUE DateTimeTargetQueue
 WITH STATUS = ON
 GO
+
+/** Following set of queues is extracted by running the query below: **
+/** SELECT DISTINCT 'CREATE QUEUE ' + QueueConsumer + ' WITH STATUS = ON' FROM [AlviandaMessageBroker].[dbo].[BrokerServicesMatrix] **/
+
+CREATE QUEUE dbo.QueueConsumerA WITH STATUS = ON
+CREATE QUEUE dbo.QueueConsumerB WITH STATUS = ON
+CREATE QUEUE dbo.QueueConsumerC WITH STATUS = ON
+CREATE QUEUE dbo.QueueConsumerD WITH STATUS = ON
+CREATE QUEUE dbo.QueueConsumerE WITH STATUS = ON
+GO
