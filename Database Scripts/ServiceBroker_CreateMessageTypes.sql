68c488a07692d413f799599db045156e47acb0cb
Anton Georgescu <antongeorgescu@gmail.com>
2021-03-13 17:28:31 -0500

    Update ServiceBroker_CreateMessageTypes.sql

--- Database Scripts/ServiceBroker_CreateMessageTypes.sql
+++ Database Scripts/ServiceBroker_CreateMessageTypes.sql
@@ -12,3 +12,13 @@ CREATE MESSAGE TYPE
 [mhb.cbs.DateTimeResponseMessage]
 VALIDATION = WELL_FORMED_XML
 GO
+
+/** following set of messages is created by running the query below **/
+/ **/** SELECT DISTINCT 'CREATE MESSAGE TYPE [' + TargetMessageType + '] VALIDATION = WELL_FORMED_XML' FROM [AlviandaMessageBroker].[dbo].[BrokerContractMessageTypes] **/
+
+CREATE MESSAGE TYPE [mhb.cbs.ResponseMessageType1] VALIDATION = WELL_FORMED_XML
+CREATE MESSAGE TYPE [mhb.cbs.ResponseMessageType2] VALIDATION = WELL_FORMED_XML
+CREATE MESSAGE TYPE [mhb.cbs.ResponseMessageType3] VALIDATION = WELL_FORMED_XML
+CREATE MESSAGE TYPE [mhb.cbs.ResponseMessageType4] VALIDATION = WELL_FORMED_XML
+CREATE MESSAGE TYPE [mhb.cbs.ResponseMessageType5] VALIDATION = WELL_FORMED_XML
+GO
