73deab5f4b8a5296dff0244c7616a31d9c88b4b9
Anton Georgescu <antongeorgescu@gmail.com>
2021-03-13 18:15:15 -0500

    Update ServiceBroker_CreateContracts.sql

--- Database Scripts/ServiceBroker_CreateContracts.sql
+++ Database Scripts/ServiceBroker_CreateContracts.sql
@@ -8,11 +8,11 @@ CREATE CONTRACT [mhb.cbs.DateTimeContract]
 GO
 
 /** following set of contracts is created by running the query below **/
-/** SELECT DISTINCT 'CREATE CONTRACT ['+[Contract]+'] (['+InitiatorMessageType+'] SENT NY INITIATOR,['+TargetMessageType+'] SENT BY TARGET )'  FROM [AlviandaMessageBroker].[dbo].[BrokerContractMessageTypes] **/
+/** SELECT DISTINCT 'CREATE CONTRACT ['+[Contract]+'] (['+InitiatorMessageType+'] SENT BY INITIATOR,['+TargetMessageType+'] SENT BY TARGET )'  FROM [AlviandaMessageBroker].[dbo].[BrokerContractMessageTypes] **/
 
-CREATE CONTRACT [mhb.cbs.ContractInType1] ([mhb.cbs.InRequestMessage] SENT NY INITIATOR,[mhb.cbs.ResponseMessageType1] SENT BY TARGET )
-CREATE CONTRACT [mhb.cbs.ContractInType2] ([mhb.cbs.InRequestMessage] SENT NY INITIATOR,[mhb.cbs.ResponseMessageType2] SENT BY TARGET )
-CREATE CONTRACT [mhb.cbs.ContractInType3] ([mhb.cbs.InRequestMessage] SENT NY INITIATOR,[mhb.cbs.ResponseMessageType3] SENT BY TARGET )
-CREATE CONTRACT [mhb.cbs.ContractInType4] ([mhb.cbs.InRequestMessage] SENT NY INITIATOR,[mhb.cbs.ResponseMessageType4] SENT BY TARGET )
-CREATE CONTRACT [mhb.cbs.ContractInType5] ([mhb.cbs.InRequestMessage] SENT NY INITIATOR,[mhb.cbs.ResponseMessageType5] SENT BY TARGET )
+CREATE CONTRACT [mhb.cbs.ContractInType0] ([mhb.cbs.InRequestMessage] SENT BY INITIATOR,[mhb.cbs.ResponseMessageType1] SENT BY TARGET )
+CREATE CONTRACT [mhb.cbs.ContractInType1] ([mhb.cbs.InRequestMessage] SENT BY INITIATOR,[mhb.cbs.ResponseMessageType2] SENT BY TARGET )
+CREATE CONTRACT [mhb.cbs.ContractInType2] ([mhb.cbs.InRequestMessage] SENT BY INITIATOR,[mhb.cbs.ResponseMessageType3] SENT BY TARGET )
+CREATE CONTRACT [mhb.cbs.ContractInType3] ([mhb.cbs.InRequestMessage] SENT BY INITIATOR,[mhb.cbs.ResponseMessageType4] SENT BY TARGET )
+CREATE CONTRACT [mhb.cbs.ContractInType4] ([mhb.cbs.InRequestMessage] SENT BY INITIATOR,[mhb.cbs.ResponseMessageType5] SENT BY TARGET )
 GO
